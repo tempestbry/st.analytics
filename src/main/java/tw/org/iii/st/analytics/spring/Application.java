fa09cf6da19b0cc8403d9225edf534c99deaa28c
ansonliu <ansonliu@.iii.org.tw>
2015-05-28 17:29:23 +0800

    add c3p0

--- src/main/java/tw/org/iii/st/analytics/spring/Application.java
+++ src/main/java/tw/org/iii/st/analytics/spring/Application.java
@@ -1,23 +1,57 @@
 package tw.org.iii.st.analytics.spring;
 
+import java.beans.PropertyVetoException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
-@ComponentScan({"tw.org.iii.st.analytics.controller"})
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@ComponentScan({ "tw.org.iii.st.analytics.controller" })
 @EnableAutoConfiguration
 public class Application extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+	@Autowired
+	Environment environment;
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
+	@Bean(name = "datasource")
+	public ComboPooledDataSource dataSource() throws PropertyVetoException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setDriverClass(environment
+				.getRequiredProperty("c3p0.driver"));
+		dataSource.setJdbcUrl(environment.getRequiredProperty("c3p0.url"));
+		dataSource.setUser(environment.getRequiredProperty("c3p0.user"));
+		dataSource
+				.setPassword(environment.getRequiredProperty("c3p0.password"));
+		dataSource.setInitialPoolSize(environment.getRequiredProperty(
+				"c3p0.initialPoolSize", Integer.class));
+		dataSource.setMaxPoolSize(environment.getRequiredProperty(
+				"c3p0.maxPoolSize", Integer.class));
+		dataSource.setMinPoolSize(environment.getRequiredProperty(
+				"c3p0.minPoolSize", Integer.class));
+		dataSource.setAcquireIncrement(environment.getRequiredProperty(
+				"c3p0.acquireIncrement", Integer.class));
+		dataSource.setMaxStatements(environment.getRequiredProperty(
+				"c3p0.maxStatements", Integer.class));
+		dataSource.setMaxIdleTime(environment.getRequiredProperty(
+				"c3p0.maxIdleTime", Integer.class));
+		return dataSource;
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
